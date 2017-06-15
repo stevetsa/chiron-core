7eaedbc9e7473611432049954eb6fd8d7d115c2a
stevetsa <mylagimail2004@yahoo.com>
2017-06-15 11:34:01 -0400

    add all files

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+IMAGE_VERSION='1.1.3'
+
+docker rmi -f umigs/chiron-core
+docker build --no-cache -t umigs/chiron-core:latest -t umigs/chiron-core:${IMAGE_VERSION} .
+docker images
+
+echo "If ready for release, run: "
+echo "  docker push umigs/chiron-core:latest"
+echo "  docker push umigs/chiron-core:${IMAGE_VERSION}"
+
---
+++ setup.R
@@ -0,0 +1,1 @@
+install.packages(c('getopt', 'reshape2', 'dplyr'), repos='http://cran.revolutionanalytics.com/')
